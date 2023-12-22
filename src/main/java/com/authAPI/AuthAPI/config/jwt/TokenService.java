009be6cc22ad52f53cf814f72f40bf820c724930
Júlio César <juliocfds20@gmail.com>
2023-12-21 23:59:18 -0300

    jwtAuthFilter added

--- src/main/java/com/authAPI/AuthAPI/config/jwt/TokenService.java
+++ src/main/java/com/authAPI/AuthAPI/config/jwt/TokenService.java
@@ -3,6 +3,7 @@ package com.authAPI.AuthAPI.config.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.authAPI.AuthAPI.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,11 @@ public class TokenService {
 
     public String generateToken(User user){
         try {
-
             String token = JWT.create()
-                    .withIssuer("auth-api")
-                    .withSubject(user.getUsername())
-                    .withExpiresAt(generateExpirationToken())
-                    .sign(algorithm());
+                    .withIssuer("auth-api")//emissor
+                    .withSubject(user.getUsername())//define o assunto do token
+                    .withExpiresAt(generateExpirationToken())//define a data de expiração do token
+                    .sign(algorithm());//garante que o token não foi modificado por alguém não autorizado
             return token;
 
         }catch (JWTCreationException exception){
@@ -32,7 +32,19 @@ public class TokenService {
         }
     }
 
-    public Instant generateExpirationToken(){
+    public String validateToken(String token){
+        try {
+            return JWT.require(algorithm())
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+    public Instant generateExpirationToken(){//calcula e retorna a data de expiracao
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
--- src/main/java/com/authAPI/AuthAPI/config/security/WebSecurityConfig.java
+++ src/main/java/com/authAPI/AuthAPI/config/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +29,9 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                 .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/product/**").hasRole("ADMIN")
-                .anyRequest().permitAll());
+                .anyRequest().authenticated()
+
+        ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
